c7d8ed208931ed2617374ace4d1328de6af92a3f
cody <hi@codywhitby.net>
2020-05-01 13:02:16 -0400

    vueeeee

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function getJSON(url, qs_params) {
 
 getJSON("https://spreadsheets.google.com/feeds/list/12vqpLpmehBzF4S28IpylhjUlK7Js2YyWdV6WCoNYP28/od6/public/values?alt=json")
 .then(data => {
-console.log(data.feed.entry);
+// console.log(data.feed.entry);
 });
 
     new Vue({
@@ -40,11 +40,11 @@ console.log(data.feed.entry);
           getJSON("https://spreadsheets.google.com/feeds/list/12vqpLpmehBzF4S28IpylhjUlK7Js2YyWdV6WCoNYP28/od6/public/values?alt=json").then(data => {
               console.log(data);
               // get status
-              // response.status;
-              // this.allMen = response.data.posts.items;
+              // data.status;
+              this.sigs = data.feed.entry;
           })
-          }, function (response) {
-
+          }, function (data) {
+            console.warn(data);
               // error callback
           }
         },
