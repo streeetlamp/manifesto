2aa44b15f8a2316b56a660e314541c7a4838e045
cody <hi@codywhitby.net>
2020-05-01 12:05:28 -0400

    this?

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-    new Vue({
-      el: '#app',
-      data: {
-        allMen: [],
-      },
-      created () {
-        this.fetchData();
-      },
-      methods: {
-        fetchData: function() {
-          // GET request
-          this.$http.get('https://spreadsheets.google.com/feeds/list/12vqpLpmehBzF4S28IpylhjUlK7Js2YyWdV6WCoNYP28/od6/public/values?alt=json').then(function (response) {
-            console.log(response);
-              // get status
-              response.status;
-              // this.allMen = response.data.posts.items;
+function getJSON(url, qs_params) {
+  function buildQueryString(params) {
+    return Object.entries(params).map(d => `${d[0]}=${d[1]}`).join('&');
+  }
 
-          }, function (response) {
+  return new Promise((resolve, reject) => {
+    const qs = qs_params ? '?' + buildQueryString(qs_params) : '';
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${url}${qs}`);
 
-              // error callback
-          })
-        },
+    xhr.onload = function() {
+      if (xhr.status >= 200 && xhr.status < 400) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        resolve(xhr.responseText);
       }
-    })
+    };
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
+}
+
+
+getJSON("https://spreadsheets.google.com/feeds/list/12vqpLpmehBzF4S28IpylhjUlK7Js2YyWdV6WCoNYP28/od6/public/values?alt=json")
+.then(data => {
+console.log(data);
+});
